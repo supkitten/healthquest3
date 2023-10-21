a0470724988ff92ef6cd80f2556547ca1fb32440
Justin <99409558+JustinJ2006@users.noreply.github.com>
2023-10-21 17:37:24 -0400

    bg

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -25,6 +25,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.info);
     }
     public void start(View a) {setContentView(R.layout.patord);}
-    //public void checkbox(View b) {}
+    public void checkbox(View b) {}
     public void doc(View a) { setContentView(R.layout.doctor); }
 }
